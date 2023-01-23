8b06754225dd1be9edde84e0c1aaba1c1f96535c
Bruno Bessa <bebessax@gmail.com>
2023-01-23 15:51:42 -0300

    corrigindo a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-    const dataDoEvento = new Date("Jan 2, 2023 19:00:00");
+    const dataDoEvento = new Date("Jan 28, 2023 19:00:00");
     const timeStampDoEvento = dataDoEvento.getTime();
 
     const contaAsHoras = setInterval(function() {
